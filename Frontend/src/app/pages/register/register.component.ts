fd4d864b60dffd8b3b675fe5bf7fa91aec9b3142
Fernando <fernandoweisheim@hotmail.com>
2023-03-29 14:12:29 -0300

    Trabajando register

--- Frontend/src/app/pages/register/register.component.ts
+++ Frontend/src/app/pages/register/register.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { faHome, faLaptop,faHeart  } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-
+  faHome=faHome;
+  faLaptop=faLaptop;
+  faHeart=faHeart;
 }
