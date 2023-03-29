7c1f31f1e926492b39fb71c9221e662379f13224
Fernando <fernandoweisheim@hotmail.com>
2023-03-29 00:56:40 -0300

    Trabajando login

--- Frontend/src/app/pages/login/login.component.ts
+++ Frontend/src/app/pages/login/login.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { faHome, faLaptop,faHeart  } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  faHome=faHome;
+  faLaptop=faLaptop;
+  faHeart=faHeart;
 
 }
