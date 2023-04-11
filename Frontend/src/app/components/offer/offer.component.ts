ac63501a31e306743640a54b1a1ffe6fb42de166
Elcira Ibarra <elcira.rpi@gmail.com>
2023-04-10 22:56:21 -0300

    Formulario de ofertas

--- Frontend/src/app/components/offer/offer.component.ts
+++ Frontend/src/app/components/offer/offer.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { faB } from '@fortawesome/free-solid-svg-icons';
 import { Offer } from 'src/app/models/offers.model';
 
 @Component({
@@ -6,29 +8,41 @@ import { Offer } from 'src/app/models/offers.model';
   templateUrl: './offer.component.html',
   styleUrls: ['./offer.component.css']
 })
-export class OfferComponent {
+export class OfferComponent implements OnInit {
 
-  offers: Offer[] = [{
-    cargo: '',
-    ubicacion: '',
-    sueldo: 0,
-    descripcion: '',
-    telefono: 0,
-    logo: ''
-  }];
+  newOffer: FormGroup = new FormGroup({
 
-  newOffer: Offer = {
-    cargo: '',
-    ubicacion: '',
-    sueldo: 0,
-    descripcion: '',
-    telefono: 0,
-    logo: ''
+  });
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+
+  }
+
+  offers: Offer[] = [];
+
+  ngOnInit() {
+    this.newOffer = this.fb.group(
+      {
+        cargo: ['', Validators.required],
+        ubicacion: ['', Validators.required],
+        sueldo: [0, Validators.required],
+        descripcion: ['', Validators.required],
+        telefono: [0, Validators.required],
+        logo: ['', Validators.required],
+      }
+    )
+    console.log('newOffer', this.newOffer.value)
   }
 
 
+
   createOffer() {
-    this.offers.push(this.newOffer);
+    if (this.newOffer.valid) {
+      this.offers.push(this.newOffer.getRawValue());
+      console.log('offers',this.offers)
+    }
   }
 
 }
