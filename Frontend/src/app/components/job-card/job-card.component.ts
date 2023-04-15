7f705f2286f494db86a646f812c5863f00a92e7a
Federico <federicoburgos.dev@gmail.com>
2023-04-15 05:45:37 -0300

    condicionados editar y eliminar

--- Frontend/src/app/components/job-card/job-card.component.ts
+++ Frontend/src/app/components/job-card/job-card.component.ts
@@ -1,29 +1,38 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { faLocationDot, faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
 import { JobsService } from 'src/app/services/jobs.service';
 import { EditJobOfferComponent } from '../edit-job-offer/edit-job-offer.component';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-job-card',
   templateUrl: './job-card.component.html',
   styleUrls: ['./job-card.component.css']
 })
-export class JobCardComponent {
+export class JobCardComponent implements OnInit {
 
   @Input() offer!: any;
   faLocationDot = faLocationDot;
   faEllipsisVertical = faEllipsisVertical;
 
+  loggedUserId: any = this.auth.idUsuarios()
+
   user = {
     'img': '../../../assets/img/face-profile.png'
   }
 
-  isUserOffer: boolean = true;
+  isUserOffer: boolean = false;
   editDeleteControls: boolean = false;
 
-  constructor(public dialog: MatDialog, private jobsService: JobsService) {
+  constructor(public dialog: MatDialog, private jobsService: JobsService, private auth: AuthService) {
 
+  }
+
+  ngOnInit(): void {
+    if (this.offer.user_id == this.loggedUserId) {
+      this.isUserOffer = true;
+    }
   }
 
   editDeleteToggle() {
