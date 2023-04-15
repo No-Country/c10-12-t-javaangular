83b79a46c1b2bc4f23f8abe45a9863c6532db4dc
Federico <federicoburgos.dev@gmail.com>
2023-04-15 13:44:54 -0300

    trabajando about / member card

--- Frontend/src/app/pages/about-us/components/members-card/members-card.component.ts
+++ Frontend/src/app/pages/about-us/components/members-card/members-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-members-card',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./members-card.component.css']
 })
 export class MembersCardComponent {
+
+  @Input() member: any; 
 
 }
--- Frontend/src/app/pages/about-us/components/members/members.component.ts
+++ Frontend/src/app/pages/about-us/components/members/members.component.ts
@@ -7,4 +7,70 @@ import { Component } from '@angular/core';
 })
 export class MembersComponent {
 
+  pmtl: any[] = [
+    {
+      'name': 'Juan Pablo Gil Jota',
+      'role': 'Project Manager',
+      'img': '../../../../../assets/img/team-members/juan-pablo.png',
+      'linkedin': '',
+      'quote': 'upa',
+    },
+    {
+      'name': 'Diego A. Muñoz',
+      'role': 'Team Leader',
+      'img': '../../../../../assets/img/team-members/diego.png',
+      'linkedin': ''
+    },
+  ]
+
+  frontend: any[] = [
+    {
+      'name': 'Elcira Ibarra',
+      'role': 'Frontend Dev',
+      'img': '../../../../../assets/img/team-members/elcira.png',
+      'linkedin': '',
+      'quote': '"Amo codear desde que tengo 7 años. Me encanta trabajar de lo que amo."',
+    },
+    {
+      'name': 'Federico Burgos',
+      'role': 'Frontend Dev',
+      'img': '../../../../../assets/img/team-members/federico.png',
+      'linkedin': '',
+      'quote': '"Amo codear desde que tengo 7 años. Me encanta trabajar de lo que amo."',
+    },
+    {
+      'name': 'Fernando Weisheim',
+      'role': 'Frontend Dev',
+      'img': '../../../../../assets/img/team-members/fernando.png',
+      'linkedin': '',
+      'quote': '"Amo codear desde que tengo 7 años. Me encanta trabajar de lo que amo."',
+    },
+  ];
+
+  design: any[] = [
+    {
+      'name': 'Celina Owen',
+      'role': 'Diseñadora UX/UI',
+      'img': '../../../../../assets/img/team-members/celina.png',
+      'linkedin': '',
+      'quote': '"Amo codear desde que tengo 7 años. Me encanta trabajar de lo que amo."',
+    },
+    {
+      'name': 'Luis Vasquez',
+      'role': 'Diseñadora UX/UI',
+      'img': '../../../../../assets/img/team-members/luis.png',
+      'linkedin': '',
+      'quote': '"Amo codear desde que tengo 7 años. Me encanta trabajar de lo que amo."',
+    },
+    {
+      'name': 'Paula Gareis',
+      'role': 'Diseñadora UX/UI',
+      'img': '../../../../../assets/img/team-members/paula.png',
+      'linkedin': '',
+      'quote': '"Amo codear desde que tengo 7 años. Me encanta trabajar de lo que amo."',
+    },
+  ];
+
+
+
 }
