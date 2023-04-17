d05679dda5c847478d0a236a7ba8d203e201a674
Federico <federicoburgos.dev@gmail.com>
2023-04-17 19:34:37 -0300

    responsive mixto

--- Frontend/src/app/components/services/services.component.ts
+++ Frontend/src/app/components/services/services.component.ts
@@ -12,19 +12,41 @@ export class ServicesComponent {
 
   services = [
     {
+      'img': '../../../assets/img/tramites.png',
+      'titulo': 'Trámites legales',
+      'descripcion': 'Recursos y guías para migrantes.',
+      'url': 'https://www.argentina.gob.ar/interior/migraciones',
+    },
+    {
+      'img': '../../../assets/img/transporte.png',
+      'titulo': 'Medios de transporte',
+      'descripcion': 'Descubrí cómo viajar por el país.',
+      'url': 'https://www.argentina.gob.ar/transporte',
+    },
+    {
+      'img': '../../../assets/img/salud.png',
+      'titulo': 'Centros de salud',
+      'descripcion': 'Conocé los lugares de emergencia.',
+      'url': '/https://buenosaires.gob.ar/establecimientos-hospitales-y-centros-de-salud',
+    },
+
+    {
+      'img': '../../../assets/img/empleos.png',
       'titulo': 'Sitios de empleos',
+      'descripcion': 'Encontrá tu nuevo y proóximo trabajo.',
       'url': '/jobs',
-      'img': '../../../assets/img/empleos.png'
     },
     {
+      'img': '../../../assets/img/couchsurfing.png',
       'titulo': 'Couchsurfing',
+      'descripcion': 'Alojamiento gratuito en hogar local.',
       'url': '/alojamiento',
-      'img': '../../../assets/img/couchsurfing.png'
     },
     {
+      'img': '../../../assets/img/comunidad.png',
       'titulo': 'Comunidad',
+      'descripcion': 'Saber dónde contratar servicios.',
       'url': '/comunidad',
-      'img': '../../../assets/img/comunidad.png'
     }
   ]
 
